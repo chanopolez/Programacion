5f0d5236f86754669fc45579e854959c31b978b9
chanopolez <81364616+chanopolez@users.noreply.github.com>
2021-05-07 20:06:08 -0300

    Add files via upload

---
+++ Perro.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1tlp;
+
+/**
+ *
+ * @author nacho
+ */
+public class Perro {
+    private String nombre;
+    private int edad;
+    private String raza;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String n) {
+        this.nombre = n;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int e) {
+        this.edad = e;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+    public void setRaza(String r) {
+        this.raza = r;
+    }    
+}
